af01ebb0d18c92209a4f241a07dc4bdb4fded7cb
Hạnh Nhi <136316165+myolshie@users.noreply.github.com>
2024-03-12 22:15:09 +0700

    ui: admin dashboard
    textblock + listview (fake data)
    
    toi da that bai voi livechart, syncfusionchart TT

--- MovieManagement/Views/Admin_Main.xaml.cs
+++ MovieManagement/Views/Admin_Main.xaml.cs
@@ -5,8 +5,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using MovieManagement.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -21,11 +23,33 @@ namespace MovieManagement.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+    public class TopTen
+    {
+        public string Title { get; set; }
+        public string ImageLocation { get; set; }
+        public int TicketCount { get; set; }
+        // Other properties
+        public TopTen(string image, string title, int count)
+        {
+            this.ImageLocation = image;
+            this.Title = title;
+            this.TicketCount = count;
+        }
+    }
     public sealed partial class Admin_Main : Page
     {
+        public ObservableCollection<TopTen> myList { get; } = new ObservableCollection<TopTen>();
+        // Other properties and methods
+
         public Admin_Main()
         {
             this.InitializeComponent();
+            myList.Add(new TopTen("ms-appx:///Assets/thumbnail-ngang.jpg", "Dao", 1));
+            myList.Add(new TopTen("ms-appx:///Assets/thumbnail-ngang.jpg", "Mai", 4));
+            myList.Add(new TopTen("ms-appx:///Assets/thumbnail-ngang.jpg", "Daoeee", 5));
+            myList.Add(new TopTen("ms-appx:///Assets/thumbnail-ngang.jpg", "Dao", 8));
+
         }
     }
 }
