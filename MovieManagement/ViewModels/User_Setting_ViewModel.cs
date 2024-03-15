6f7a706c9dc129dcdc60038c66e888ae46d5808e
Thế Nghĩa <nghiask2018@gmail.com>
2024-03-15 20:30:46 +0700

    feat: Setup ViewModels for User

---
+++ MovieManagement/ViewModels/User_Setting_ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MovieManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace MovieManagement.ViewModels
+{
+    class User_Setting_ViewModel
+    {
+
+    }
+}
