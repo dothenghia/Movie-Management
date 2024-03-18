63d2ea00ff076504d6c4a0ddbb7211098eee8ff5
Le Thanh Trung <letrung148257@gmail.com>
2024-03-18 17:44:31 +0700

    create view model base

--- MovieManagement/ViewModels/User_Movie_ViewModel.cs
+++ MovieManagement/ViewModels/User_Movie_ViewModel.cs
@@ -8,8 +8,75 @@ using System.Collections.ObjectModel;
 
 namespace MovieManagement.ViewModels
 {
-    class User_Movie_ViewModel
+    public class User_Movie_ViewModel:ViewModelBase
     {
+        private ObservableCollection<ShowTime> _showtimes;
+        private ObservableCollection<Ticket> _tickets;
+        public ObservableCollection<ShowTime> ShowTimes
+        {
+            get
+            {
+                return _showtimes;
+            }
+            set
+            {
+                _showtimes = value;
+                NotifyPropertyChanged(nameof(ShowTimes));
+            }
+        }
+        public ObservableCollection<Ticket> Tickets
+        {
+            get
+            {
+                return _tickets;
+            }
+            set
+            {
+                _tickets = value;
+                NotifyPropertyChanged(nameof(Tickets));
+            }
+        }
 
+        public User_Movie_ViewModel()
+        {
+            _tickets = new ObservableCollection<Ticket>()
+            {
+                 new Ticket()
+                {
+                    IsAvailable = true,
+                    Row= "A",
+                    Col= 1,
+                    Price= 1,
+                },
+                 new Ticket()
+                {
+                    IsAvailable = true,
+                    Row= "A",
+                    Col= 1,
+                    Price= 1,
+                },
+                 new Ticket()
+                {
+                    IsAvailable = true,
+                    Row= "A",
+                    Col= 1,
+                    Price= 1,
+                },
+                 new Ticket()
+                {
+                    IsAvailable = true,
+                    Row= "A",
+                    Col= 1,
+                    Price= 1,
+                }
+            };
+            _showtimes = new ObservableCollection<ShowTime>
+            {
+                new ShowTime()
+                {
+
+                }
+            };
+        }
     }
 }
