f9413407df47ba686800b7fa431515db7c010ad5
Thế Nghĩa <nghiask2018@gmail.com>
2024-03-21 00:49:14 +0700

    feat: Create global context for application

---
+++ MovieManagement/GlobalContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement
+{
+    public class GlobalContext
+    {
+        public static int UserID = 0; // 0 means user did not login yet
+        public static Boolean Go2Setting = false; // Fix Navigate to Setting page after login bug
+
+        public static void SetUserID(int id) {
+            UserID = id;
+        }
+
+        public static void SetGo2Setting(Boolean go2Setting) {
+            Go2Setting = go2Setting;
+        }
+    }
+}
